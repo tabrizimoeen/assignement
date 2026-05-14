07aa3a6bd509810dd506a04f6bf5c6ffafd714e0
m.tabrizi <moeentabrizi@gmail.com>
2026-05-15 01:35:02 +0330

    [feat] Add Project Structure and Dependencies

---
+++ src/main/java/org/imdb/platform/technicalassignment/TechnicalAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.imdb.platform.technicalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalAssignmentApplication.class, args);
+    }
+
+}
